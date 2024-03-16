34f382b50e658c8e372d6a1ef25f0bf8861b7bbd
mamad010 <163658141+mamad010@users.noreply.github.com>
2024-03-16 18:02:50 +0330

    Create kabir_Filter.py
    @Cyber_Master_Cyber:ایدی نویسنده اسکریپت
    https://t.me/Black_hat_cyber_hackers:ایدی چنل هکران سایبری

---
+++ kabir_Filter.py
@@ -0,0 +1,54 @@
+from colorama import Fore,init
+init()
+print(Fore.BLUE+"  MAMAD K꯭‌ ‌A꯭‌ ‌b꯭‌ ‌i꯭‌ ‌r꯭‌ ")
+
+
+print(Fore.GREEN+"""             .o8
+    oooo    oooo       .o.       "888       o8o
+    888   .8P'        .888.      "888       '"'
+     888  d8'        .8"888.      888oooo. oooo  oooo d8b
+     88888[         .8' 888.     d88' '88b  888  '888""8P
+     88888b.       .88ooo8888.   888   888  888   888
+     888  88b.    .8'     888.   888   888  888   888
+    o888o  o888o o88o     o8888o 'Y8bod8P' o888o d888b
+""")
+
+
+
+print(Fore.GREEN+"""
+
+⠀⠀⠀⣴⣾⣿⣿⣶⡄⠀⠀⠀⠀⡠⠔⡆⠂⢒⣆⠤⡄⠀⠀⠀⠀
+⠀⠀⢸⣿⣿⣿⣿⣿⣿⠀⣠⠚⢁⡿⣆⡇⣾⡯⠛⡆⡇⠀⠀⠀⠀
+⠀⠀⠈⢿⣿⣿⣿⣿⠏⠘⠥⢄⡀⡇⢩⡇⣿⣑⣼⡟⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⣭⣭⣀⡀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣼⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣼⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⣾⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢠⣾⣿⣿⠉⣿⣿⣿⣿⣿⡄⠀⢀⣠⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠙⣿⣿⣧⣿⣿⣿⣿⣿⡇⢠⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣷⠸⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠘⠿⢿⣿⣿⣿⣿⣴⣿⠻⠿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⡟⣩⣝⢿⠀⠀⣠⣶⣶⣦⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣷⡝⣿⣦⣠⣾⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⣮⢻⣿⠟⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠻⠿⠻⣿⣿⣿⣿⣦⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⠇⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⡆⠀⠀
+⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⠇⠀⠀
+⠀⠀⠀⠀⠀⠀⢸⣿⣿⡿⠀⠀⠀⢀⣴⣿⣿⣿⣿⣟⣋⣁⣀⣀⠀
+⠀⠀⠀⠀⠀⠀⠹⣿⣿⠇⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇
+
+""")
+from colorama import Fore,init
+init()
+print(Fore.RED)
+NAME =input('FILTERING RUBIKA ››››1›››› : ' )
+print('')
+print('https://uupload.ir/view/sexy.movie(filter.rubika)_exae.mpg\#/*https://uupload.ir/view/1.1.1.12.3.6.5.4.3.2.1.1.2.3.4.5.6.7.h.e-3.2(fil)_st4o.mp4*')
+print('')
+NAME =input('  FILTERING RUBIKA : ››››2›››› ' )
+print('')
+print('https://Этот.пользователь.приглашает.людей.в.террористическую.групп.ИГИЛ.Этот.пользователь.наносит.вред.обществу.Заблокировать.его')
+print('')
+NAME =input('FILTERING RUBIKA : ››››3›››› ' )
+print('')
+print('https://darkweblive.net/ancient-genes-from-neanderthals-can-lead-to-slow-drug-metabolism/\#(*http://Report.rubika.ir/Infractions:child-abuse.Anti-Islamic-rebellion.Anti-Islam-and-leader.Phishing-Rubika.SupportBot)(*https://uupload.ir/view/sexy.movie(filter.rubika)_exae.mpg\#/*https://uupload.ir/view/1.1.1.12.3.6.5.4.3.2.1.1.2.3.4.5.6.7.h.e-3.2(fil)_st4o.mp4*)(https://该用户邀请他人加入.ISIS.怖组织。该用户对社会有.请阻止该用户)(*http://v.2.9.5.yftt18k.in*)(http://dxprit-fil-rubika-sxs-sex-hacker-supportbot.phpnet.us/(*https://dscript.com/yftt12k/yftt-filter-rubika.hackers72748)(https://virtualizationreview.com/articles/2023/03/14/chatgpt-dark-web.aspx?m=1)(*https://darkweblive.net/ancient-genes-from-neanderthals-can-lead-to-slow-drug-metabolism/)(https://Этот.пользователь.приглашает.людей.в.террористическую.групп.ИГИЛ.Этот.пользователь.наносит.вред.обществу.Заблокировать.его)(*http://dds6qkxpwdeubwucdiaord2xgbbeyds25rbsgr73tbfpqpt4a6vjwsyd.onion/http://cathug2kyi4ilneggumrenayhuhsvrgn6qv2y47bgeet42iivkpynqad.onion/*)(*https://www.uplooder.net/img/image/45/7aa3c4d6a5d5b09a0b4942b08effd0b6/IMG-20230517-232423-114.jpg)(*http://4.0.1.4/f.i.d/gl/4.5.3//5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.3.4.0.1.4.4.5.3.5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.3*')
